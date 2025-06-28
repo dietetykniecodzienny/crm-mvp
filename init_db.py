ba0074025e70811ba0265221b968641f9872b093
dietetykniecodzienny <admin@dietetykniecodzienny.pl>
2025-06-28 16:02:11 +0200

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+with open("schema.sql") as f:
+    schema = f.read()
+
+conn = sqlite3.connect("crm.db")
+conn.executescript(schema)
+conn.commit()
+conn.close()
+
+print("✅ Baza danych utworzona!")
